efdb50c0dda87b3cdd29059952836e98d6dd22b6
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-04-29 18:28:26 +0300

    Authentication & Authorization

--- app3/resolvers/Mutation/auth.ts
+++ app3/resolvers/Mutation/auth.ts
@@ -7,25 +7,25 @@ import { JWT_SIGNATURE } from '../../src/keys';
 
 interface SignUpArgs {
     credentials: {
-        email: string;
-        password: string;
+        email: string,
+        password: string,
     }
-    bio: string;
-    name: string;
+    bio: string,
+    name: string,
 }
 
 interface SigninArgs {
     credentials: {
-        email: string;
-        password: string;
+        email: string,
+        password: string,
     };
 }
 
 interface UserPayload {
-    userErors: {
-        message: string
-    }[];
-    token: string | null;
+    userErrors: {
+        message: string,
+    }[],
+    token: string | null,
 }
 
 export const authResolvers = {
@@ -38,7 +38,7 @@ export const authResolvers = {
         })
         if (!isValidPassword) {
             return {
-                userErors: [{
+                userErrors: [{
                     message: "Invalid password",
                 }],
                 token: null
@@ -46,7 +46,7 @@ export const authResolvers = {
         }
         if (!isEmail) {
             return {
-                userErors: [{
+                userErrors: [{
                     message: "Invalid Email",
                 }],
                 token: null
@@ -54,7 +54,7 @@ export const authResolvers = {
         }
         if (!name || !bio) {
             return {
-                userErors: [{
+                userErrors: [{
                     message: "Invalid name or bio",
                 }],
                 token: null
@@ -79,7 +79,7 @@ export const authResolvers = {
         });
 
         return {
-            userErors: [],
+            userErrors: [],
             token: JWT.sign(
                 {
                     userId: user.id,
@@ -89,5 +89,48 @@ export const authResolvers = {
                     expiresIn: 360000,
                 })
         }
+    },
+    signin: async (_: any, { credentials }: SigninArgs, { prisma }: Context)
+        : Promise<UserPayload> => {
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (!user) {
+            return {
+                userErrors: [{
+                    message: "Invalid credentials",
+                }],
+                token: null
+            };
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return {
+                userErrors: [{
+                    message: "Invalid credentials",
+                }],
+                token: null
+            };
+        }
+
+        return {
+            userErrors: [],
+            token: JWT.sign(
+                {
+                    userId: user.id
+                },
+                JWT_SIGNATURE,
+                {
+                    expiresIn: 360000,
+                })
+        }
+
     }
 }
