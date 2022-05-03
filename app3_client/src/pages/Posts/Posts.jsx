87af9c48dc1f4c6b98ef3a349b65077679eb3c3e
Vadym Boyarchuk <vadimca1.boyarchuc@gmail.com>
2022-05-03 19:00:41 +0300

    connect apollo-server, client

--- app3_client/src/pages/Posts/Posts.jsx
+++ app3_client/src/pages/Posts/Posts.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import Post from "../../components/Post/Post";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      title
+      content
+      user {
+        name
+      }
+  }
+  }
+`
 
 export default function Posts() {
+
+  const {data, error, loading } = useQuery(GET_POSTS);
+
   return <div></div>;
 }
